33e78a3edccd6b094d5ef96ef42bce271127014a
Adam Young <atyoung1997@gmail.com>
2023-12-25 20:50:35 -0600

    add basic land distribution calculator

--- screwed.py
+++ screwed.py
@@ -6,9 +6,10 @@ import re
 import requests
 import sys
 
+from math import comb
 from time import sleep
 
-SCRYFALL_REQUEST_URL = "https://api.scryfall.com/cards/named?exact="
+SCRYFALL_REQUEST_URL = "https://api.scryfall.com"
 test_path_1 = "C:\\Users\\atyou\\github_repos\\screwed\\testing_files\\archidekt_standard_export_1.txt"
 test_path_2 = "C:\\Users\\atyou\\github_repos\\screwed\\testing_files\\archidekt_standard_export_2.txt"
 
@@ -33,23 +34,67 @@ def readAndFormatDecklist(input_path):
 
 def retrieveScryfallData(deck_dict):
     for card_name in deck_dict.keys():
-        request_string = f"{SCRYFALL_REQUEST_URL}{card_name.replace(" ", "+")}"
+        request_string = f"{SCRYFALL_REQUEST_URL}/cards/named?exact={card_name.replace(" ", "+")}"
         scryfall_response = requests.get(request_string)
         scryfall_response_json = scryfall_response.json()
         deck_dict[card_name]["mana_cost"] = scryfall_response_json["mana_cost"]
         deck_dict[card_name]["cmc"] = scryfall_response_json["cmc"]
         deck_dict[card_name]["type_line"] = scryfall_response_json["type_line"]
         deck_dict[card_name]["oracle_text"] = scryfall_response_json["oracle_text"]
-        print(json.dumps(scryfall_response_json, indent = 1))
         sleep(0.075)
     return deck_dict
+
+def getLandCount(deck_dict):
+    land_count = 0
+    for card_name in deck_dict.keys():
+        if re.search(r"( Land | Land$|^Land |^Land$)", deck_dict[card_name]["type_line"]):
+            land_count += deck_dict[card_name]["count"]
+    return land_count
+
+def getCardCount(deck_dict):
+    card_count = 0
+    for card_name in deck_dict.keys():
+        card_count += deck_dict[card_name]["count"]
+    return card_count
+
+def getLandDistribution(card_count, land_count, num_cards_drawn):
+    nonland_count = card_count - land_count
+    prob_list = []
+    for num_lands_drawn in range(0, num_cards_drawn+1):
+        prob = comb(land_count, num_lands_drawn) * comb(nonland_count, num_cards_drawn - num_lands_drawn) / comb(card_count, num_cards_drawn)
+        prob_list.append(prob)
+        print(f"You have a {prob:.0%} chance to draw {num_lands_drawn} lands in {num_cards_drawn} draws.")
+    cum_prob_list = []
+    inv_cum_prob_list = []
+    cum_prob = 0
+    for prob in prob_list:
+        cum_prob += prob
+        cum_prob_list.append(cum_prob)
+        inv_cum_prob_list.append(1-cum_prob)
+    
+    df = pd.DataFrame({
+        "num_lands_drawn": range(0, num_cards_drawn+1),
+        "prob": prob_list,
+        "cum_prob": cum_prob_list,
+        "inv_cum_prob": inv_cum_prob_list
+        }
+    )
+    return df
+        
 
 
 
 if __name__ == "__main__":
-    deck_dict_1 = readAndFormatDecklist(test_path_1)
-    deck_dict_1 = retrieveScryfallData(deck_dict_1)
-    print(json.dumps(deck_dict_1, indent=1))
+    deck_dict = readAndFormatDecklist(test_path_1)
+    deck_dict = retrieveScryfallData(deck_dict)
+    land_count = getLandCount(deck_dict)
+    card_count = getCardCount(deck_dict)
+    print(f"Your deck has: {land_count} lands in it out of {card_count} total cards. This means {land_count/card_count:.0%} of your deck is made up of lands.")
+    getLandDistribution(card_count, land_count, 9)
+
+
+
+    #print(json.dumps(deck_dict, indent=1))
 
 
 
