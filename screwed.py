5d7ead3adf9261ca8aef4b7bc4bc71f1a934009c
Adam Young <atyoung1997@gmail.com>
2023-12-31 23:12:14 -0600

    updates

--- screwed.py
+++ screwed.py
@@ -2,6 +2,7 @@ import json
 import numpy as np
 import os
 import pandas as pd
+import pickle
 import re
 import requests
 import sys
@@ -31,7 +32,6 @@ def readAndFormatDecklist(input_path: str):
         card_name = " ".join(split_line[1:])
         
         deck_dict[card_name] = {"count": card_count}
-    print(deck_dict)
     return deck_dict
 
 def retrieveScryfallData(deck_dict: dict):
@@ -59,6 +59,15 @@ def getCardCount(deck_dict: dict):
         card_count += deck_dict[card_name]["count"]
     return card_count
 
+def getAverageCMC(deck_dict: dict):
+    card_count = 0
+    total_cmc = 0
+    for card_name in deck_dict.keys():
+        card_count += deck_dict[card_name]["count"]
+        total_cmc += deck_dict[card_name]["cmc"] * deck_dict[card_name]["count"]
+    return total_cmc / card_count
+
+
 def getLandDistribution(card_count: int, land_count: int, num_cards_drawn: int):
     nonland_count = card_count - land_count
     prob_list = []
@@ -66,7 +75,7 @@ def getLandDistribution(card_count: int, land_count: int, num_cards_drawn: int):
         prob = comb(land_count, num_lands_drawn) * comb(nonland_count, num_cards_drawn - num_lands_drawn) / comb(card_count, num_cards_drawn)
         prob_list.append(prob)
 
-    return np.array(prob_list)
+    return prob_list
 
 def playLandEachTurn(card_count: int, land_count: int, turn_number: int, starting_hand_size: int):
     num_cards_drawn_at_turn = turn_number - 1 + starting_hand_size
@@ -74,26 +83,55 @@ def playLandEachTurn(card_count: int, land_count: int, turn_number: int, startin
     print(np.sum(land_dist[turn_number:]))
 
 def analyzeMulligans(card_count: int, land_count: int, min_lands_keep: int = 3, max_lands_keep: int = 5, hard_stop: int = 4, starting_hand_size: int = 7):
-    mull_analysis_dict = {num_cards_drawn: {"land_dist": getLandDistribution(card_count, land_count, num_cards_drawn)} for num_cards_drawn in range(1, starting_hand_size + 1)}
-    
+    mull_land_dist_list = [getLandDistribution(card_count, land_count, num_cards_drawn) for num_cards_drawn in range(starting_hand_size, 0, -1)]
     if min_lands_keep > starting_hand_size or min_lands_keep < 0:
         raise ValueError(f"min_lands_keep must be a value between 0 and {starting_hand_size} (starting_hand_size)")
     if max_lands_keep > starting_hand_size or max_lands_keep < 1:
         raise ValueError(f"max_lands_keep must be a value between 1 and {starting_hand_size} (starting_hand_size)")
 
-    #print(json.dumps(mull_analysis_dict, indent = 1))
-    for num_cards_drawn in mull_analysis_dict.keys():
-        mull_analysis_dict[num_cards_drawn]["keep_prob"] = mull_analysis_dict[num_cards_drawn]["land_dist"][min_lands_keep : max_lands_keep + 1]
-        mull_analysis_dict[num_cards_drawn]["mull_prob"] = 1 - mull_analysis_dict[num_cards_drawn]["keep_prob"]
-        #print(mull_analysis_dict[num_cards_drawn]["land_dist"][:max_lands_keep])
-    # hand_size = starting_hand_size
-    # while hand_size >= min_lands_keep:
-    #     land_dist = 
+    # calculate mull and keep probability for each number of cards drawn
+    keep_prob_list, mull_prob_list = [], []
+    for land_dist in mull_land_dist_list:
+        if land_dist == []:
+            keep_prob = 0
+        else:
+            keep_prob = sum(land_dist[min_lands_keep : max_lands_keep + 1])
+        keep_prob_list.append(keep_prob)
+        mull_prob_list.append(1 - keep_prob)
+    print(keep_prob_list)
+    print(mull_prob_list)
 
-    if starting_hand_size <= min_lands_keep:
-        return 1
+    # probability of mulling exactly a certain number of times
+    for num_mulls in range(len(keep_prob_list)):
+        if num_mulls == 0:
+            exact_mull_prob_list = [keep_prob_list[num_mulls]]
+        else:
+            exact_mull_prob_list.append(np.prod(mull_prob_list[0:num_mulls]) * keep_prob_list[num_mulls])
     
-    print("helllo")
+    # based on parameters, you may reach a point where mulling further will make it impossible to meet the param requirements
+    prob_find_keepable_mull = sum(exact_mull_prob_list)
+    if prob_find_keepable_mull < 1:
+        i = len(exact_mull_prob_list) - 1
+        while i >= 0:
+            if exact_mull_prob_list[i] != 0:
+                last_possible_mull = i
+                break
+            i -= 1
+    print(f"With {land_count} lands in a {card_count} card deck, if you mull until you hit {min_lands_keep}-{max_lands_keep} lands, there will be a {1 - prob_find_keepable_mull:.2%} chance that you mull {last_possible_mull} times and never hit the required lands.")
+    print(f"{last_possible_mull=}")
+    
+    
+
+    print(f"{exact_mull_prob_list=}")
+    print(f"{sum(exact_mull_prob_list)=}")
+        
+
+
+
+    
+        
+    
+    # print(json.dumps(mull_analysis_dict, indent = 1))
 
 def playLandEachTurnWithMulligan(card_count: int, land_count: int, turn_number: int, mull_to: int):
     print("tbd")
@@ -102,22 +140,22 @@ def playXLandsInYTurns(card_count: int, land_count: int, lands_played: int, turn
     print("tbd")
 
 if __name__ == "__main__":
-    deck_dict = readAndFormatDecklist(test_path_1)
-    deck_dict = retrieveScryfallData(deck_dict)
+    # deck_dict = readAndFormatDecklist(test_path_1)
+    # deck_dict = retrieveScryfallData(deck_dict)
+    # deck_dict.keys()
+    # print(deck_dict)
+    # with open("C:\\Users\\atyou\\github_repos\\screwed\\testing_files\\formatted_file.p", 'wb') as formatted_file:   
+    #     pickle.dump(deck_dict, formatted_file)
+    with open("C:\\Users\\atyou\\github_repos\\screwed\\testing_files\\formatted_file.p", 'rb') as formatted_file:
+        deck_dict = pickle.load(formatted_file)
+    # print(deck_dict[list(deck_dict.keys())[0]])
+    # print(json.dumps(deck_dict, indent = 1))
     land_count = getLandCount(deck_dict)
     card_count = getCardCount(deck_dict)
-    print(f"Your deck has: {land_count} lands in it out of {card_count} total cards. This means {land_count/card_count:.0%} of your deck is made up of lands.")
-    opening_hand_land_dist = getLandDistribution(card_count, land_count, 7)
-    print("Your opening hand land draw probabilities are: ")
-    print(opening_hand_land_dist)
-    print("If you mull to 6 the distribution changes to this:")
-    single_mull_land_dist = getLandDistribution(card_count, land_count, 6)
-    print(single_mull_land_dist)
 
-    playLandEachTurn(card_count, land_count, 4, 7)
-    playLandEachTurn(card_count, land_count, 4, 8)
     analyzeMulligans(card_count, land_count, min_lands_keep=2, max_lands_keep=5, hard_stop=4, starting_hand_size=7)
-
+    analyzeMulligans(card_count, land_count, min_lands_keep=3, max_lands_keep=5, hard_stop=4, starting_hand_size=7)
+    print(getAverageCMC(deck_dict))
     #print(json.dumps(deck_dict, indent=1))
 
 
